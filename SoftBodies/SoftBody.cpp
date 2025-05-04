81bcaf70cb0cc48b3562de378f6b085e2ea9b9e8
Ewan-DuplessisK <148007352+Ewan-DuplessisK@users.noreply.github.com>
2025-05-04 19:54:10 +0200

    Added ground collision & more complex body

--- SoftBodies/SoftBody.cpp
+++ SoftBodies/SoftBody.cpp
@@ -16,6 +16,19 @@ void SoftBody::updateActor(float dt){
     //std::cout<<Vector3(points[1].position-points[0].position).length()<<std::endl;
 }
 
+bool SoftBody::pointMassGroundCollisions(Point* p){
+    auto& groundPlane = getGame().getPlanes()[0];
+    const AABB& planeBox = groundPlane->getBox()->getWorldBox();
+    if(Collisions::intersect(Sphere{p->position,5},planeBox)){
+        //std::cout<<"planecollisions"<<std::endl;
+        p->force.z = /*planeBox.max.z - (p->position.z - 5.f)*/9*p->mass;
+        p->position.z = planeBox.max.z + 5.f;
+        //std::cout<<p->position.z<<std::endl;
+        return true;
+    }
+    return false;
+}
+
 /*void SoftBody::applyUniformMovement(float dt){
     for(auto p:points){
         Vector3 tSpeed = p.speed + uniformMovement;
@@ -107,18 +120,119 @@ void SoftBody::Solve(float dt){
         points[i].speed+=points[i].force*dt * (1/points[i].mass);
         points[i].position+=points[i].speed*dt;
     }
+    //float tmp = (points[0].position-points[2].position).length();
+    //std::cout<<tmp<<std::endl;
 }
 
 SoftBody::SoftBody(){
-    points.emplace_back(Point{Vector3{0.f,0.f,50.f},Vector3(),Vector3(),2});
-    points.emplace_back(Point{Vector3{0.f,0.f,30.f},Vector3(),Vector3(),2});
+    //Hexagon
+    points.emplace_back(Point{Vector3{0,.5f*20.f,3.5f/4.f*20.f+30.f},Vector3(),Vector3(),2});
+    points.emplace_back(Point{Vector3{0,1.f*20.f,30.f},Vector3(),Vector3(),2});
+    points.emplace_back(Point{Vector3{0,.5f*20.f,-3.5f/4.f*20.f+30.f},Vector3(),Vector3(),2});
+    points.emplace_back(Point{Vector3{0,-.5f*20.f,-3.5f/4.f*20.f+30.f},Vector3(),Vector3(),2});
+    points.emplace_back(Point{Vector3{0,-1.f*20.f,30.f},Vector3(),Vector3(),2});
+    points.emplace_back(Point{Vector3{0,-.5f*20.f,3.5f/4.f*20.f+30.f},Vector3(),Vector3(),2});
+
+    //Base cycle
     Spring s;
     s.points[0]=&points[0];
     s.points[1]=&points[1];
     s.baseDistance=10.f;
-    s.stiffness=1.f;
+    s.stiffness=10.f;
     s.damping=1.f;
     springs.emplace_back(s);
-    
+    Spring s2;
+    s2.points[0]=&points[1];
+    s2.points[1]=&points[2];
+    s2.baseDistance=10.f;
+    s2.stiffness=10.f;
+    s2.damping=1.f;
+    springs.emplace_back(s2);
+    Spring s3;
+    s3.points[0]=&points[2];
+    s3.points[1]=&points[3];
+    s3.baseDistance=10.f;
+    s3.stiffness=10.f;
+    s3.damping=1.f;
+    springs.emplace_back(s3);
+    Spring s4;
+    s4.points[0]=&points[3];
+    s4.points[1]=&points[4];
+    s4.baseDistance=10.f;
+    s4.stiffness=10.f;
+    s4.damping=1.f;
+    springs.emplace_back(s4);
+    Spring s5;
+    s5.points[0]=&points[4];
+    s5.points[1]=&points[5];
+    s5.baseDistance=10.f;
+    s5.stiffness=10.f;
+    s5.damping=1.f;
+    springs.emplace_back(s5);
+    Spring s6;
+    s6.points[0]=&points[5];
+    s6.points[1]=&points[0];
+    s6.baseDistance=10.f;
+    s6.stiffness=10.f;
+    s6.damping=1.f;
+    springs.emplace_back(s6);
+
+
+    //Pyramid
+    /*points.emplace_back(Point{Vector3{0,0,50.f},Vector3(),Vector3(),2});
+    points.emplace_back(Point{Vector3{28.f,0.f,30.f},Vector3(),Vector3(),2});
+    points.emplace_back(Point{Vector3{-10.f,10.f,30.f},Vector3(),Vector3(),2});
+    points.emplace_back(Point{Vector3{-10.f,-10.f,30.f},Vector3(),Vector3(),2});
+
+    Spring s0;
+    s0.points[0]=&points[0];
+    s0.points[1]=&points[1];
+    s0.baseDistance=25.f;
+    s0.stiffness=10.f;
+    s0.damping=2.f;
+    springs.emplace_back(s0);
+    Spring s1;
+    s1.points[0]=&points[0];
+    s1.points[1]=&points[2];
+    s1.baseDistance=25.f;
+    s1.stiffness=10.f;
+    s1.damping=2.f;
+    springs.emplace_back(s1);
+    Spring s2;
+    s2.points[0]=&points[0];
+    s2.points[1]=&points[2];
+    s2.baseDistance=25.f;
+    s2.stiffness=10.f;
+    s2.damping=2.f;
+    springs.emplace_back(s2);
+    Spring s3;
+    s3.points[0]=&points[0];
+    s3.points[1]=&points[3];
+    s3.baseDistance=25.f;
+    s3.stiffness=10.f;
+    s3.damping=2.f;
+    springs.emplace_back(s3);
+
+    Spring s4;
+    s4.points[0]=&points[1];
+    s4.points[1]=&points[2];
+    s4.baseDistance=25.f;
+    s4.stiffness=10.f;
+    s4.damping=2.f;
+    springs.emplace_back(s4);
+    Spring s5;
+    s5.points[0]=&points[2];
+    s5.points[1]=&points[3];
+    s5.baseDistance=25.f;
+    s5.stiffness=10.f;
+    s5.damping=2.f;
+    springs.emplace_back(s5);
+    Spring s6;
+    s6.points[0]=&points[3];
+    s6.points[1]=&points[1];
+    s6.baseDistance=25.f;
+    s6.stiffness=10.f;
+    s6.damping=2.f;
+    springs.emplace_back(s6);*/
 }
 
